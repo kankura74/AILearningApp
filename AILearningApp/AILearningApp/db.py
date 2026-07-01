697edb33e81ba82ef451543f8125a4e2d52cf236
長谷川 栞奈 <hasegawa@sicnet.local>
2026-07-01 10:58:07 +0900

    Move database configuration to environment variables

--- AILearningApp/AILearningApp/db.py
+++ AILearningApp/AILearningApp/db.py
@@ -1,13 +1,10 @@
 import psycopg2
-
+from AILearningApp import DB_HOST, DB_NAME, DB_USER, DB_PASSWORD
 
 def get_connection():
-
-    conn = psycopg2.connect(
-        host="localhost",
-        database="ai_learning_app",
-        user="postgres",
-        password="sicadmin"
+    return psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD
     )
-
-    return conn
